4aa90205d58abfead19264d4bfceeb16d9007741
Jeffrey Noel Quilaquil <jeffrey.quilaquil@gmail.com>
2018-11-17 17:41:20 +0800

    I dont know about this

--- application/views/applySignature.php
+++ application/views/applySignature.php
@@ -4,5 +4,30 @@
   </div>
   <div class="card-body">
 
+    <iframe src="<?= $this->config->base_url()?>uploads/profiles/<?= $signatoryInfo['username'].'/'.$signatoryInfo['title'] ?>.pdf" width="100%" height="450px"></iframe>
   </div>
 </div>
+<img src="<?= $this->config->base_url().'uploads/signatures/'.$this->user->username ?>.png" alt="" id="signature">
+<script type="text/javascript">
+
+  $(document).mousemove(function(e){
+    $('#signature').css({
+      left: e.clientX,
+      top:  e.clientY
+    });
+
+    console.log(e.clientX, e.clientY);
+  })
+</script>
+
+<style media="screen">
+  #signature{
+    width: 30px;
+    height: 30px;
+    position: absolute;
+  }
+
+  iframe:hover #signature{
+
+  }
+</style>
